9c14a4c50a6c6bb7ad346e189d96d48a180a4d62
rejetisomeswararao <rejetisomeswararao@gmail.com>
2025-09-27 01:13:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function first() {
+  var x = window.prompt("ENTER YOUR NAME");
+ if(x===null||x.trim() === "") {
+    document.getElementById("welcome").innerHTML = "welcome to garena";
+  } else {
+    document.getElementById("welcome").innerHTML = x +" "+ "welcome to garena";
+  }
+}
+first();
