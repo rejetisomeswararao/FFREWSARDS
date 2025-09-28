988e16e60e41f9951daa4dd0516d96d4aaffffe4
rejetisomeswararao <rejetisomeswararao@gmail.com>
2025-09-28 17:11:04 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function first() {
-  var x = window.prompt("ENTER YOUR NAME");
- if(x===null||x.trim() === "") {
-    document.getElementById("welcome").innerHTML = "welcome to garena";
-  } else {
-    document.getElementById("welcome").innerHTML = x +" "+ "welcome to garena";
-  }
-}
-first();
